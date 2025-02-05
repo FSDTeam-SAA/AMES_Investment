ffe774478a7b01c5b3c9f74db59d9023065b132e
abu-said <abubdcalling@gmail.com>
2025-02-05 15:49:05 +0600

    mysql change

--- config/database.php
+++ config/database.php
@@ -45,11 +45,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '5.tcp.ngrok.io'),
-            'port' => env('DB_PORT', '25981'),
-            'database' => env('DB_DATABASE', 'ais_db'),
-            'username' => env('DB_USERNAME', 'Abir'),
-            'password' => env('DB_PASSWORD', 'AbirDev!'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -61,6 +61,26 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DB_URL'),
+        //     'host' => env('DB_HOST', '5.tcp.ngrok.io'),
+        //     'port' => env('DB_PORT', '25981'),
+        //     'database' => env('DB_DATABASE', 'ais_db'),
+        //     'username' => env('DB_USERNAME', 'Abir'),
+        //     'password' => env('DB_PASSWORD', 'AbirDev!'),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => env('DB_CHARSET', 'utf8mb4'),
+        //     'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
 
         'mariadb' => [
             'driver' => 'mariadb',
